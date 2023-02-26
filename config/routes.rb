6a12b8212631c5542d1d0bee52f28cbe0d44d2b8
Manuela Baecker <manu.baecker@t-online.de>
2023-02-26 16:27:06 +0100

    add dotenv-rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # get 'lists/index'
-  # root 'lists#index'
+  root 'lists#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
